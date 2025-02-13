f6876bb2d45da94f4718044ca4ad2abac31cd2ac
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-13 15:48:27 +0700

    refactor authMiddleware to use REFRESH_TOKEN_SECRET and update error response format

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -5,16 +5,16 @@ const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const token = req.header("Authorization")?.split(" ")[1];
 
   if (!token) {
-    res.status(401).json({ message: "Unauthorized: No token provided" });
+    res.status(401).json({ errors: "Unauthorized: No token provided" });
     return;
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    const decoded = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET as string);
     (req as any).user = decoded; // Menyimpan user yang terautentikasi di request
     next();
   } catch (error) {
-    res.status(401).json({ message: "Unauthorized: Invalid token" });
+    res.status(401).json({ errors: "Unauthorized: Invalid token" });
     return;
   }
 };
