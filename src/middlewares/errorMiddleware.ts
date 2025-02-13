3b1d71b59551689982365c21382b8e31e38ba320
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-13 15:48:44 +0700

    update errorMiddleware to include NextFunction parameter for improved error handling

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
-const errorMiddleware = (err: any, req: Request, res: Response) => {
+const errorMiddleware = (err: any, req: Request, res: Response, next: NextFunction) => {
   const status = err.status || 500;
   const message = err.message || 'Something went wrong.';
   if (process.env.NODE_ENV === 'production') {
